bf7cc09799d0c951f6d56c0c3fb50db9adeb916a
WELKANG <welkang@qq.com>
2015-11-24 09:11:07 +0800

    servlet get params

--- Basic/source/production/java/com/wrox/HelloServlet.java
+++ Basic/source/production/java/com/wrox/HelloServlet.java
@@ -4,11 +4,13 @@ package com.wrox;
  * Created by jiaoxin on 15/11/23.
  */
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet
 {
@@ -16,6 +18,14 @@ public class HelloServlet extends HttpServlet
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         response.getWriter().println("Hello World!");
+
+        // 获取参数
+        ServletConfig c = this.getServletConfig();
+        PrintWriter writer = response.getWriter();
+
+        writer.append("database: ").append(c.getInitParameter("database"))
+                .append(", server: ").append(c.getInitParameter("server"));
+
     }
 
     @Override
--- readme.md
+++ readme.md
@@ -1,2 +1,31 @@
 Servlet是一个运行在Web服务器中的Java小程序。
 接收和响应来自Web客户端的HTTP请求。
+
+通过web.xml配置servlet mapping来将路由和servlet关联起来。
+
+servlet处理页面展示还是太弱，所以需要引入jsp，负责view的展示。
+
+
+servlet默认参数配置
+方式一：web.xml
+    <servlet>
+        <servlet-name>helloServlet</servlet-name>
+        <servlet-class>com.wrox.HelloServlet</servlet-class>
+        <init-param>
+            <param-name>database</param-name>
+            <param-value>Mongodb</param-value>
+        </init-param>
+        <init-param>
+            <param-name>server</param-name>
+            <param-value>10.2.2.2</param-value>
+        </init-param>
+    </servlet>
+方式二：servlet注解(简洁集中，但是多个实例、特定执行顺序时无法满足)
+	@WebServlet(
+        name = "contextParameterServlet",
+        urlPatterns = {"/contextParameters"},
+        initParams = {
+        	@WebInitParams(name = "database", value = "Mongodb"),
+        	@WebInitParams(name = "server", value = "10.10.1.1")
+    	}
+	)
